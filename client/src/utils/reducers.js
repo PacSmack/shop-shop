095bf45318ff03a83b32b579537f47ed25aa8cd9
Nathan da Silva <pacsmacck@gmail.com>
2021-08-05 17:53:32 -0700

    update current category reducer

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -6,11 +6,24 @@ export const reducer = (state, action) => {
         case UPDATE_PRODUCTS:
             return {
                 ...state,
-                products: [...action.products]
-            };
+                products: [...action.products],
+            }
 
-            // IF IT'S NONE OF THESE ACTIONS, DO NOT UPDATE STATE AT ALL AND KEEP THINGS THE SAME!
-            default:
-                return state;
+        //if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories],
+            }
+
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory
+            }
+
+        // IF IT'S NONE OF THESE ACTIONS, DO NOT UPDATE STATE AT ALL AND KEEP THINGS THE SAME!
+        default:
+            return state
     }
 }
